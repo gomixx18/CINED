b8e26023ada986766c07120168c88476a5c85334
gomixx18 <bryanr9418@hotmail.com>
2016-03-01 20:48:52 -0600

    funciones actualizado

--- js/funciones.js
+++ js/funciones.js
@@ -1,16 +1,42 @@
 var contador = 1;
 
-function agregarEstudiante() {
-        var aux = $("#estudiantesTFG >*").length;
+function agregarInvest() {
+        
+        var aux = $("#investigadores").find("div").length;
         if(aux<6){
-        $("#estudiantesTFG").append("<div id=\"idEstudiante" + contador + "\"><br> <input required class=\"form-control\" name=\"idEstudiante" + contador + " id=\"idEstudiante" + contador + "></div>");
-        $("#botonesTFG").append("<div id=\"idEstudiante" + contador + "\"></br><button id=\"idEstudiante" + contador + "\" type=\"button\" class=\"btn btn-default\" aria-label=\"Left Align\" onclick=\"eliminarEstudiante(this)\"><span class=\"glyphicon glyphicon-minus\" aria-hidden=\"true\"></span></button></div>");
+        $("#investidadores").append("<div id=\"divInvest"+contador+"\" class=\"form-group\"><label  for=\"inputInvest"+contador+"\">Cédula Investigador:</label name=\"inputInvest"+contador+"\"><input id=\"idInvestigador" + contador + "\" class=\"form-control input-sm m-b-xs required\" placeholder=\"cédula investigador\" name=\"idInvestigador" + contador + " id=\"idEstudiante" + contador + "\"> \n\
+         <button id=\"divInvest" + contador + "\" type=\"button\" class=\"btn btn-danger btn-rounded \" onclick=\"eliminarInvest(this)\">Eliminar Investigador</button></div>");
         contador++;
     }
 }
 
-function eliminarEstudiante(event) {
+function eliminarInvest(event) {
+    
+    alert(contador)
+    $("div").remove("#divInvest"+contador);
+    if(contador > 1){
+        contador-- ;
+    }
+    
+    
+    
+    
+}
+
+
+function agregarEstudiantes() {
+        var aux = $("#estudiantes").length;
+        if(aux<6){
+        $("#estudiantes").append("<div id=\"divEstud"+contador+"\" class=\"form-group\"><label  for=\"inputEstud"+contador+"\">Cédula Estudiante:</label name=\"inputestud"+contador+"\"><input id=\"idEStudiante" + contador + "\" class=\"form-control input-sm m-b-xs required\" placeholder=\"cédula Estudiante\" name=\"idEstudiante" + contador + " id=\"idEstudiante" + contador + "\"> \n\
+         <button id=\"divEstud" + contador + "\" type=\"button\" class=\"btn btn-danger btn-rounded \" onclick=\"eliminarEstudiantes(this)\">Eliminar Estudiante</button></div>");
+        contador++;
+    }
+}
+
+function eliminarEstudiantes(event) {
     $("div").remove("#"+event.id);
     
     
 }
+
+
