fb2717d9e8eb3195d2db83d13995c86a6ba6083f
Brenda Ruiz <brendaruizm@hotmail.com>
2016-03-13 19:07:50 -0600

    Tildes

--- agregar_proyecto_extension.php
+++ agregar_proyecto_extension.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Administración Extensión</title>
+    <title>Administración Proyectos de Extensión</title>
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
