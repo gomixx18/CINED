6334469a32ac6a84abf01e7ca69ef5f8228aa126
PastorCR <isakucorrales@hotmail.es>
2016-04-30 17:10:08 -0600

    Added files via upload

---
+++ funcionalidad/TFGTitulo.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+$tfg = $_REQUEST["tfg"];
+$titulo = $_REQUEST["titulo"];
+
+$connection = mysqli_connect("localhost", "root", "cined123", "uned_db");
+
+if ($connection) {
+    $consulta = "update uned_db.tfg set titulo = '$titulo' where codigo = '$tfg'";
+    $query = mysqli_query($connection, $consulta);
+    mysqli_close($connection);
+}
